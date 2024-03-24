bdc16458c5626074177c646e1a5e7f0ed0b05b24
Julien Bourque <julien@brqu.me>
2024-03-24 15:26:19 -0400

    Update...

--- Database/deploy.sh
+++ Database/deploy.sh
@@ -7,10 +7,10 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-RUN DEBIAN_FRONTEND=noninteractive apt-get update && apt-get upgrade -y && apt-get install -y postgresql
+RUN DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y postgresql
 
-cp ./configs/pg_ident.conf /etc/postgresql/14/main/pg_ident.conf
-cp ./configs/pg_hba.conf /etc/postgresql/14/main/pg_hba.conf
-cp ./configs/postgress.conf /etc/postgresql/14/main/postgress.conf
+cp ./configs/pg_ident.conf /etc/postgresql/12/main/pg_ident.conf
+cp ./configs/pg_hba.conf /etc/postgresql/12/main/pg_hba.conf
+cp ./configs/postgress.conf /etc/postgresql/12/main/postgress.conf
 
 sudo systemctl restart postgresql.service
