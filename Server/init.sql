b6a264fb612f1614c481c78a43dd6f5f441b65fa
Julien Bourque <julien@brqu.me>
2024-03-24 17:23:35 -0400

    Updated sql file

--- Server/init.sql
+++ Server/init.sql
@@ -1,3 +1,18 @@
+DO
+$do$
+BEGIN
+   IF EXISTS (
+      SELECT FROM pg_catalog.pg_roles
+      WHERE  rolname = 'toto') THEN
+
+      RAISE NOTICE 'Role "toto" already exists. Skipping.';
+   ELSE
+      CREATE ROLE toto LOGIN PASSWORD 'unGrosMotDePasse';
+   END IF;
+END
+$do$;
+
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   username VARCHAR (50) UNIQUE NOT NULL,
